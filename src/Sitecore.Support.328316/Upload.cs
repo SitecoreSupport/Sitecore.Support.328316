f95d0d4ad691581324976fdadb4c5f4469246057
MarinaRuzhilo <mru@sitecore.net>
2019-05-08 19:39:43 +0300

    Fixing the issue.

---
+++ src/Sitecore.Support.328316/Upload.cs
@@ -0,0 +1,47 @@
+﻿namespace Sitecore.Support.Shell.Framework.Commands.Media
+{
+  using Sitecore;
+  using Sitecore.Configuration;
+  using Sitecore.Data;
+  using Sitecore.Data.Items;
+  using Sitecore.Diagnostics;
+  using Sitecore.Globalization;
+  using Sitecore.Shell.Framework.Commands;
+  using Sitecore.Text;
+  using Sitecore.Web;
+  using Sitecore.Web.UI.Sheer;
+  using System;
+  using System.Collections.Specialized;
+  using System.Linq;
+  using System.Web;
+
+  [Serializable]
+  public class Upload : Sitecore.Shell.Framework.Commands.Media.Upload
+  {
+    public override void Execute(CommandContext context)
+    {
+      Assert.ArgumentNotNull(context, "context");
+      if (context.Items.Length == 1)
+      {
+        Item item = context.Items[0];
+        NameValueCollection parameters = new NameValueCollection
+        {
+          ["id"] = StringUtil.GetString(new string[] {
+                        context.Parameters["id"],
+                        item.ID.ToString()
+                    }),
+          ["language"] = HttpUtility.ParseQueryString(Sitecore.Context.Items["SC_REQUEST_MEASUREMENT_URL"].ToString()).Get("la"),
+          ["version"] = item.Version.ToString(),
+          ["load"] = StringUtil.GetString(new string[] { context.Parameters["load"] }),
+          ["edit"] = StringUtil.GetString(new string[] { context.Parameters["edit"] }),
+          ["tofolder"] = StringUtil.GetString(new string[] { context.Parameters["tofolder"] }),
+          [Sitecore.Configuration.State.Client.UsesBrowserWindowsQueryParameterName] = StringUtil.GetString(new string[] {
+                        context.Parameters[Sitecore.Configuration.State.Client.UsesBrowserWindowsQueryParameterName],
+                        "0"
+                    })
+        };
+        Context.ClientPage.Start(this, "Run", parameters);
+      }
+    }
+  }
+}
